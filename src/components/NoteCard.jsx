ceafe4df88670a5eaabb2ad54a1a866f63434987
loganathanramsaran <loguflower3@gmail.com>
2025-06-22 04:28:29 +0530

    minor changes

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -105,7 +105,7 @@ function NoteCard({ note, updateNote, setEditingNote }) {
         {note.tags?.map((tag, i) => (
           <span
             key={i}
-            className={`px-2 py-0.5 rounded-lg bg-green-700 ${
+            className={`px-2 py-0.5 rounded-lg bg-blue-600 ${
               colorMap[note.color]?.split(" ")[2] || "bg-blue-100"
             } text-white dark:text-white`}
           >
